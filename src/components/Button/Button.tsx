11f50e33a4edc82a60a6a05d298cc2cd1e331747
Marcelo Martins <m.a.martins@live.com>
2021-07-19 21:32:33 -0300

    Getting all props from a button inside of custom component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,16 @@
 import * as B from './Button.styles'
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
-export type ButtonProps = {
-  variant: 'danger' | 'text' | 'primary',
-  label: string
-}
+export type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> &
+  {
+    variant: 'danger' | 'text' | 'primary',
+    label: string
+  }
 
-export default function Button({ variant }: ButtonProps) {
+export default function Button({ label, variant, ref, ...props }: ButtonProps) {
   return (
-    <B.Wrapper variant={variant}>
-      Button
+    <B.Wrapper variant={variant} {...props}>
+      {label}
     </B.Wrapper>
   )
 }

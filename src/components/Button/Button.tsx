9bf4090b88886330f3404d4cd0c5d96a6e25b88d
Marcelo Martins <m.a.martins@live.com>
2021-12-06 17:56:12 -0300

    Adicionando estilização global (fontes) no Storybook

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,19 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import * as B from './Button.styles'
+
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  variant: 'danger' | 'text' | 'primary',
+  label: string,
+  disabled?: boolean
+}
+
+export default function Button({ variant, label, ref, ...props }: ButtonProps) {
+  return (
+    <B.Wrapper
+      variant={variant}
+      {...props}
+    >
+      {label}
+    </B.Wrapper>
+  )
+}
---
+++ src/stories/Input.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Input from "../components/Input/Input";
+
+export default {
+  title: 'Example/Input',
+  component: Input
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const WithPlaceholder = Template.bind({})
+WithPlaceholder.args = {
+  placeholder: 'e.g.: John Doe'
+}
+
+export const WithLabelAndPlaceholder = Template.bind({})
+WithLabelAndPlaceholder.args = {
+  label: 'Name',
+  placeholder: 'e.g.: John Doe'
+}
+
+export const WithLabelAndContent = Template.bind({})
+WithLabelAndContent.args = {
+  label: 'Name',
+  value: 'John'
+}
+
+export const WithContent = Template.bind({})
+WithContent.args = {
+  value: 'John'
+}

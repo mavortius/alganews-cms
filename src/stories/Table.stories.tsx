d1161406ed4bfc7d7c1321e9d4b45dfb442cc59c
Marcelo Martins <m.a.martins@live.com>
2022-04-08 15:46:10 -0300

    Recuperando dados além do accessor

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -5,11 +5,18 @@ import { Column, useTable } from "react-table";
 import { Meta } from "@storybook/react";
 import Table from "../components/Table/Table";
 
-type Data = {
-  preview: React.ReactNode,
-  col1: string,
-  col2: string,
-  actions: string
+type Post = {
+  id: number,
+  title: string,
+  views: number,
+  author: {
+    name: string,
+    avatar: string
+  },
+  conversions: {
+    thousands: number,
+    percentage: number
+  }
 }
 
 export default {
@@ -18,66 +25,91 @@ export default {
 } as Meta;
 
 export function Default() {
-  const data = useMemo<Data[]>(() => [
+  const data = useMemo<Post[]>(() => [
     {
-      col1: 'Hello',
-      col2: 'World',
-      actions: 'ações',
-      preview: <Icon
-        path={mdiOpenInNew}
-        size="14px"
-        color="#09f"
-      />
+      author: {
+        name: 'Daniel Bonifacio',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+      },
+      id: 1,
+      conversions: {
+        percentage: 64.35,
+        thousands: 607,
+      },
+      title: 'Como dobrei meu salário aprendendo somente React',
+      views: 985415
     },
     {
-      col1: 'react-table',
-      col2: 'rocks',
-      actions: 'ações',
-      preview: <Icon
-        path={mdiOpenInNew}
-        size="14px"
-        color="#09f"
-      />
+      author: {
+        name: 'Daniel Bonifacio',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+      },
+      id: 2,
+      conversions: {
+        percentage: 64.35,
+        thousands: 607,
+      },
+      title: 'React.js vs. React Native: a REAL diferença entre os dois',
+      views: 985415
     },
     {
-      col1: 'whatever',
-      col2: 'you want',
-      actions: 'ações',
-      preview: <Icon
-        path={mdiOpenInNew}
-        size="14px"
-        color="#09f"
-      />
+      author: {
+        name: 'Daniel Bonifacio',
+        avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNf0vAZLggJoZxGKpfOa3EBClHkwQmmvv9Lg&usqp=CAU'
+      },
+      id: 3,
+      conversions: {
+        percentage: 95.35,
+        thousands: 845,
+      },
+      title: 'Como dobrei meu salário aprendendo somente React',
+      views: 985415
     }
   ], [])
-  const columns = useMemo<Column<Data>[]>(() => [
+
+  const columns = useMemo<Column<Post>[]>(() => [
     {
       Header: '',
-      accessor: 'preview'
+      accessor: 'id',
+      Cell: () => <Icon path={mdiOpenInNew} size={'14px'} color={'#09f'} />
     },
     {
-      Header: 'Column 1',
-      accessor: 'col1',
+      Header: 'Artigo',
+      accessor: 'title',
       width: 320,
-      Cell: (row) => {
-        return <div style={{ textAlign: 'right' }}>{row.value}</div>
-      }
+      Cell: (props) => <div style={{ textAlign: 'left', display: 'flex', gap: 8, alignItems: 'center' }}>
+        <img width={24} height={24} src={props.row.original.author.avatar} alt={props.row.original.author.name} />
+        {props.value}
+      </div>
     },
     {
-      Header: 'Column 2',
-      accessor: 'col2',
+      Header: 'Views',
+      accessor: 'views',
       width: 320,
-      Cell: (row) => {
-        return <div style={{ textAlign: 'center' }}>{row.value}</div>
-      }
+      Cell: (props) =>
+        <div style={{ textAlign: 'right', fontWeight: 700, fontFamily: '"Roboto mono", monospace' }}>
+          {props.value.toLocaleString('pt-br')}
+        </div>
+    },
+    {
+      Header: 'Conversões',
+      accessor: 'conversions',
+      Cell: (props) =>
+        <div style={{ display: 'flex', gap: 8, fontWeight: 700, fontFamily: '"Roboto mono", monospace' }}>
+          <span>{props.value.thousands}k</span>
+          <span style={{ color: '#09f' }}>{props.value.percentage}%</span>
+        </div>
     },
     {
       Header: 'Ações',
-      accessor: 'actions'
+      Cell: () =>
+        <div style={{ textAlign: 'right' }}>
+          todo: actions
+        </div>
     }
   ], [])
 
-  const instance = useTable<Data>({ data, columns })
+  const instance = useTable<Post>({ data, columns })
 
-  return <Table<Data> instance={instance} />
+  return <Table<Post> instance={instance} />
 }

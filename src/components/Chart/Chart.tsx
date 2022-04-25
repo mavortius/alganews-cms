163c0341c811a6782c05c3345255782314d32e04
Marcelo Martins <m.a.martins@live.com>
2022-04-25 15:44:24 -0300

    Labels do ChartJS

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,7 +1,7 @@
 import { Line } from "react-chartjs-2";
 
 const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
+  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
   datasets: [
     {
       label: '# of Votes',

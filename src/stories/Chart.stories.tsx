0abe5749beda082dbbc498ae2351918ccf15362c
Marcelo Martins <m.a.martins@live.com>
2022-04-25 15:16:39 -0300

    Instalando o ChartJS

---
+++ src/stories/Chart.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Chart from "../components/Chart/Chart";
+
+export default {
+  title: 'Example/Chart',
+  component: Chart
+} as ComponentMeta<typeof Chart>;
+
+const Template: ComponentStory<typeof Chart> = (args) => <Chart {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+  title: 'Post não encontrado'
+}
